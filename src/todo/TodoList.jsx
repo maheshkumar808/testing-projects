d91bf8fc08bce9ea9872bd264e0ee245660b9ad9
maheshkumar808 <808maheshkumar@gmail.com>
2025-06-29 21:59:15 +0530

    changes in todo

--- src/todo/TodoList.jsx
+++ src/todo/TodoList.jsx
@@ -1,16 +1,37 @@
+// import React from 'react';
+// function TodoList({ data, onDelete }) {
+//   return (
+//     <div className="bg-red-500 flex justify-between ">
+//       {data.map((item, index) => (
+//         <li className="text-left " key={index + 1}>
+//           {item}{' '}
+//           <button className="cursor-pointer" onClick={() => onDelete(index)}>
+//             ✖️
+//           </button>
+//         </li>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default TodoList;
 import React from 'react';
+
 function TodoList({ data, onDelete }) {
   return (
-    <div>
+    <ol className=" space-y-2 ">
       {data.map((item, index) => (
-        <li className="text-left " key={index + 1}>
-          {item}{' '}
+        <li
+          className="flex justify-between items-center  p-2  border-2 rounded-xl"
+          key={index}
+        >
+          <span className="text-left">{item}</span>
           <button className="cursor-pointer" onClick={() => onDelete(index)}>
             ✖️
           </button>
         </li>
       ))}
-    </div>
+    </ol>
   );
 }
 
--- src/todo/index.jsx
+++ src/todo/index.jsx
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
 import InputBox from './inputBox';
 import TodoList from './TodoList';
+import Swal from 'sweetalert2';
 
 const index = () => {
   const [listTodo, setListTodo] = useState([]);
   let addList = (inputText) => {
+    if (!inputText.trim()) {
+      Swal.fire({
+        title: 'Empty Todo!',
+        text: 'Please enter a todo item.',
+        icon: 'warning',
+      });
+      return;
+    }
     setListTodo([...listTodo, inputText]);
   };
 
@@ -14,7 +23,7 @@ const index = () => {
   };
   return (
     <div className="bg-blue-500 w-full h-screen flex justify-center ">
-      <div className="container max-w-lg mx-auto p-7 my-20  rounded-lg text-center ">
+      <div className="container max-w-sm mx-auto p-7 my-20  rounded-lg text-center border-2 hover:shadow-amber-800 hover:shadow-2xl">
         <h1 className="text-5xl font-bold">To-do-list</h1>
         <div className="mt-6 ">
           <InputBox addList={addList} />

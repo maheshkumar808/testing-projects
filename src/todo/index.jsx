3110b4ad5ee4e9e1f116bbe7394af41c98377017
maheshkumar808 <808maheshkumar@gmail.com>
2025-09-08 19:12:06 +0530

    modify todolist

--- src/todo/index.jsx
+++ src/todo/index.jsx
@@ -1,39 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import InputBox from './inputBox';
-import TodoList from './TodoList';
-import Swal from 'sweetalert2';
-
-const index = () => {
-  const [listTodo, setListTodo] = useState([]);
-  let addList = (inputText) => {
-    if (!inputText.trim()) {
-      Swal.fire({
-        title: 'Empty Todo!',
-        text: 'Please enter a todo item.',
-        icon: 'warning',
-      });
-      return;
-    }
-    setListTodo([...listTodo, inputText]);
-  };
-
-  const onDelete = (indexValue) => {
-    const filteredList = listTodo.filter((_, index) => index !== indexValue);
-    setListTodo(filteredList);
-  };
+function index() {
   return (
-    <div className="bg-blue-500 w-full h-screen flex justify-center ">
-      <div className="container max-w-sm mx-auto p-7 my-20  rounded-lg text-center border-2 hover:shadow-amber-800 hover:shadow-2xl">
-        <h1 className="text-5xl font-bold">To-do-list</h1>
-        <div className="mt-6 ">
-          <InputBox addList={addList} />
-        </div>
-        <div className="mt-6">
-          <TodoList data={listTodo} onDelete={onDelete} />
-        </div>
+    <div className="w-full h-screen bg-blend-soft-light flex justify-center items-center">
+      <div className="max-w-sm bg-blue-700 p-6 rounded-b-2xl">
+        <h1 className="text-center text-5xl text-white font-bold mb-4">
+          TodoList
+        </h1>
+        <InputBox />
       </div>
     </div>
   );
-};
+}
 
 export default index;
--- src/todo/inputBox.jsx
+++ src/todo/inputBox.jsx
@@ -1,28 +1,48 @@
 import React, { useState } from 'react';
-import Button from '../components/Button';
 import Input from '../components/Input';
+import Button from '/src/components/Button';
 
-function InputBox(props) {
+function inputBox() {
   const [inputText, setInputText] = useState('');
+  const [items, setItems] = useState([]);
+  const handleAdd = () => {
+    if (inputText.trim() == '') {
+      alert('plese enter todo');
+    }
+    if (inputText !== '') {
+      setItems([...items, inputText]);
+      setInputText('');
+    }
+  };
+  const handleDelete = (indextodelet) => {
+    setItems(items.filter((_, index) => index !== indextodelet));
+  };
   return (
-    <div className="flex  justify-center gap-4 ">
-      <Input
-        type="text"
-        placeholder="Enter your task"
-        className="input-box-todo"
-        value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
-      />
-      <Button
-        className="btn"
-        onClick={() => {
-          props.addList(inputText);
-          setInputText('');
-        }}
-      >
-        Enter
-      </Button>
+    <div className="max-w-sm">
+      <div className="flex justify-between space-x-5">
+        <Input
+          placeholder="enter your todo"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <Button onClick={handleAdd}>Add</Button>
+      </div>
+      <ul className="mt-6 space-y-4 ">
+        {items.map((item, index) => (
+          <li
+            className="p-2 flex justify-between items-top bg-amber-500  border-2 border-amber-50 rounded-b-2xl"
+            key={index}
+          >
+            <span className="flex-1 break-words  overflow-hidden mr-2">
+              {items}
+            </span>
+
+            <Button onClick={() => handleDelete(index)}>Delete</Button>
+          </li> // Use a unique key for list items
+        ))}
+      </ul>
     </div>
   );
 }
-export default InputBox;
+
+export default inputBox;
